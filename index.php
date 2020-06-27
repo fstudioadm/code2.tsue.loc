f301eb49a7264c84ba5ec6a6d66c0c95e6114b3f
fstudioadm <fstudioadm@mail.ru>
2020-06-27 20:50:04 +0500

    chat va qabul papkalari ochildi

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php echo 'Salom Dadajon'; ?>
-<?php echo 'Test'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="/chat"></a>
+        <a href="/qabul"></a>
+    </body>
+</html>
