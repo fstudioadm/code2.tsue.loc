4bc3dfc862bf2a93d74d8c75b75e48f687d1b283
fstudioadm <fstudioadm@mail.ru>
2020-06-27 20:58:40 +0500

    qabul: style index.php

---
+++ qabul/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Qabul</title>
+    </head>
+    <body>
+        <h1 class="main-h1">Qabul hujjatlarini topshirish</h1>
+    </body>
+</html>
